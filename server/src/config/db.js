e91cdb310d55bdf0d6d33c93c9ec494356dcf5bf
johannehasley-code <hasleydiessongo@gmail.com>
2026-06-25 15:40:39 +0100

    ajout de code avant le createpool

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,6 +2,14 @@
 import 'dotenv/config';
 import mysql from 'mysql2/promise';
 
+
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER
+});
+
 const pool = mysql.createPool({
   host:     process.env.DB_HOST     || 'localhost',
   port:     parseInt(process.env.DB_PORT) || 3306,
